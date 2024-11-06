d4ced426b267961114d66d8d54e9f3ec753cccfe
rockey <rockeysen000@gmail.com>
2024-11-06 18:09:16 +0530

    created first tailwind project

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+
+      colors: {
+        'primary': '#3238f2'
+      },
+
+      fontFamily: {
+        display: ['Poppins', 'sans - serif'],
+        body: ['inter', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
